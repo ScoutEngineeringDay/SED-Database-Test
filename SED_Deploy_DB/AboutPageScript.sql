30c59c493b0e93d116fd080d124f524ce1ccefb0
Walter Hiranpat <tanuguitar@gmail.com>
2018-02-28 22:20:14 -0500

    add script to update about

--- SED_Deploy_DB/AboutPageScript.sql
+++ SED_Deploy_DB/AboutPageScript.sql
@@ -5,9 +5,11 @@ CREATE TABLE SED_Database.sedUI_aboutpage(
     registrationOpenDate VARCHAR(5000),
     registrationOpenTime VARCHAR(5000),
     locationMap VARCHAR(5000),
-    forceClosed BOOLEAN
+    forceClosed BOOLEAN,
+    parkingLocation VARCHAR(5000),
+    checkingLocation VARCHAR(5000)
 );
 INSERT INTO SED_Database.sedUI_aboutpage
-	(saveDate, eventLocation, registrationOpenDate, registrationOpenTime, locationMap)
+	(saveDate, eventLocation, registrationOpenDate, registrationOpenTime, locationMap, parkingLocation, checkingLocation)
 VALUES
-	("Saturday, April 14, 2018", "McLean, Virginia", "Thursday, March 1, 2018", "7:00pm", "7515 Colshire Dr, McLean, VA 22102");
+	("Saturday, April 14, 2018", "McLean, Virginia", "Thursday, March 1, 2018", "7:00pm", "7515 Colshire Dr, McLean, VA 22102", "Park in MITRE 1 parking lot. For a map of the MITRE campus", "Check in will be located in the MITRE 1 building. ");
